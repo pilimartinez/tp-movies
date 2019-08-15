514dd309f44cf18d2df93f3445dcab5da035f592
mercedesaven <mercedes.aven@gmail.com>
2019-08-15 19:54:44 -0300

    con moment

--- script.js
+++ script.js
@@ -4,22 +4,23 @@ const initialize = () => {
 fetch('https://api.themoviedb.org/3/movie/popular?api_key=${apiKey}')
     .then(response => response.json())
     .then(res => {
-        console.log(res)
+       // console.log(res)
         popularMovies(res.results)
+        topFivePopMovies(console.log(res.results))
     })
 }
 
 const popularMovies = (popularMovies) => {
     let popularMovieDiv = document.getElementById('pop-mov')
     
-    popularMovies.forEach(e => {
+   let popularMov =  popularMovies.forEach(e => {
         
     let popularMovieTitle = document.createElement('h1')
     let popularMovieGeners = document.createElement('h2')
     let popularMovieOverview = document.createElement('p')
 
     popularMovieTitle.innerText= e.title
-    popularMovieGeners.innerText = e.release_date
+    popularMovieGeners.innerText = moment(e.release_date).format('DD/MM/YYYY')
     popularMovieOverview.innerText = e.overview
     
     popularMovieDiv.appendChild(popularMovieTitle)
@@ -27,6 +28,11 @@ const popularMovies = (popularMovies) => {
     popularMovieDiv.appendChild(popularMovieOverview)
     })
 
-
     
+   
 } 
+/* 
+const topFivePopMovies = (movies) => {
+    movies.filter(top => top <5)
+}
+ */
