24430fbc213896fffb546531dc6c123bd32bcf06
mercedesaven <mercedes.aven@gmail.com>
2019-08-23 20:01:52 -0300

    probando

--- script.js
+++ script.js
@@ -4,32 +4,33 @@ const customeFetch = (url) => {
     return fetch(endPoint+url)
     .then(response => response.json())
         }
-console.log(customeFetch("popular"))
 
-const initialize = () => {
-    customeFetch("popular")
+
+ const initialize = () => {
+    customeFetch("top_rated")
     .then(res => {
         //console.log(res)
-         popularMovies(res.results)
+         movieCategory(res.results)
         //console.log(topFiveMovies(res.results))
     })
-}
+} 
 
 
-const popularMovies = (popularMovies) => {
-    let popularMovieDiv = document.getElementById('pop-mov')
-    popularMovieDiv.classList.add('popularMovies')
-   let popularMov =  popularMovies.forEach(({title,poster_path}) => {
+
+const movieCategory = (category) => {
+    let movieDiv = document.getElementById('pop-mov')
+    movieDiv.classList.add('popularMovies')
+   let findMovies =  category.forEach(({title,poster_path}) => {
     let movie = document.createElement('div')
     movie.classList.add('movieResume')
-    let popularMovieTitle = document.createElement('p') // porque no todo puede tener H1
-    popularMovieTitle.classList.add('movieTitle')
+    let movieTitle = document.createElement('p') // porque no todo puede tener H1
+    movieTitle.classList.add('movieTitle')
     let movieImage = document.createElement('img')
     let imageContainer = document.createElement('a')
     //let popularMovieGeners = document.createElement('h2')
     //let popularMovieOverview = document.createElement('p')
     imageContainer.href = '#'
-    popularMovieTitle.innerText= title
+    movieTitle.innerText= title
     movieImage.src = `https://image.tmdb.org/t/p/w500/${poster_path}`
     movieImage.classList.add('movieImage')
     //popularMovieGeners.innerText = moment(e.release_date).format('DD/MM/YYYY')
@@ -37,54 +38,46 @@ const popularMovies = (popularMovies) => {
     
     imageContainer.appendChild(movieImage)
     movie.appendChild(imageContainer)
-    movie.appendChild(popularMovieTitle)
+    movie.appendChild(movieTitle)
     //movie.appendChild(popularMovieGeners)
     //movie.appendChild(popularMovieOverview)
-    popularMovieDiv.appendChild(movie)
+    movieDiv.appendChild(movie)
     })
 
     
    
 } 
  
-const topFiveMovies = (movies) => {
-   return movies.filter((top, i) => { if(i <5) return top})
+    const topFiveMovies = (movies) => {
+    return movies.filter((top, i) => { if(i <5) return top})
 }
  
 // searchBar
 
 const paginaActual = 1;
 
-//const textoBusqueda = (event) => {
-   // if (event.code === 'Enter') {
-//let searchValue = event.target.value;
-//return searchValue;
-  //  }
-//}
-
 var task;
 var newTask;
 
-var textoBusqueda = function(event) {
+var textoBusqueda = (event)=> {
     if (event.code === 'Enter') {
         sendTask()
     }
 }
-
-var sendTask = function() {
+var sendTask = () => {
     task = document.getElementById('searchInput');
     newTask = task.value;
 
     if (newTask !== "") {
         task.value = "";
+        console.log(newTask)
     }
 }
-const hola = "s"
 const searchBar = () => {
-    fetch('https://api.themoviedb.org/3/search/movie?api_key=${apiKey}&query=${hola}&page=${paginaActual}')
+    fetch('https://api.themoviedb.org/3/search/movie?api_key=${apiKey}&query=${newTask}&page=${paginaActual}')
         .then(response => response.json())
         .then(res => {
-          // console.log(res)
+           console.log(res)
         })
     }
 
