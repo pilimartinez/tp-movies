a3d3063da7ea8fbd6934efc59b0910ac5bb8b6ea
pilimartinez <pilartcast@hotmail.com>
2019-08-23 21:39:32 -0300

    anda el nav

--- script.js
+++ script.js
@@ -5,57 +5,43 @@ const customeFetch = (url) => {
     .then(response => response.json())
         }
 
-
- const initialize = () => {
-    customeFetch("top_rated")
-    .then(res => {
-        //console.log(res)
-         movieCategory(res.results)
-        //console.log(topFiveMovies(res.results))
-    })
-} 
-
-
-
 const movieCategory = (category) => {
     let movieDiv = document.getElementById('pop-mov')
+    movieDiv.innerHTML = ''
     movieDiv.classList.add('popularMovies')
    let findMovies =  category.forEach(({title,poster_path}) => {
     let movie = document.createElement('div')
     movie.classList.add('movieResume')
-    let movieTitle = document.createElement('p') // porque no todo puede tener H1
+    let movieTitle = document.createElement('p')
     movieTitle.classList.add('movieTitle')
     let movieImage = document.createElement('img')
     let imageContainer = document.createElement('a')
-    //let popularMovieGeners = document.createElement('h2')
-    //let popularMovieOverview = document.createElement('p')
     imageContainer.href = '#'
     movieTitle.innerText= title
     movieImage.src = `https://image.tmdb.org/t/p/w500/${poster_path}`
     movieImage.classList.add('movieImage')
-    //popularMovieGeners.innerText = moment(e.release_date).format('DD/MM/YYYY')
-    //popularMovieOverview.innerText = e.overview
-    
     imageContainer.appendChild(movieImage)
     movie.appendChild(imageContainer)
     movie.appendChild(movieTitle)
-    //movie.appendChild(popularMovieGeners)
-    //movie.appendChild(popularMovieOverview)
     movieDiv.appendChild(movie)
-    })
-
-    
-   
+    })   
 } 
- 
     const topFiveMovies = (movies) => {
     return movies.filter((top, i) => { if(i <5) return top})
 }
  
+    // nav bar
+    const searchCategory = (url) => {
+        fetch (`https://api.themoviedb.org/3/movie/${url}?api_key=${apiKey}`)
+        .then(response => response.json())
+        .then(res => {
+            movieCategory(res.results)
+        })
+    }
+
 // searchBar
 
 const paginaActual = 1;
-
 var task;
 var newTask;
 
